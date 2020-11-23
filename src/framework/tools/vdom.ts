339b15aa9eaa86d704215a778270b139d0d77c82
sergeantO <morgueheadman@gmail.com>
2020-11-23 03:50:52 +0300

    added array to template

--- src/framework/tools/vdom.ts
+++ src/framework/tools/vdom.ts
@@ -1,57 +1,9 @@
 import { $ } from './dom'
 import { _ } from './util';
 
-export function vdom (str: string): string {
-  let lines = str.split(/\r\n|\r|\n/);
-  let elements = lines.map( line => { 
-    let elem = parseLine(line)
-      
-    if ( !_.isUndefined(elem) ) {
-      return elem 
-    }
-  }).filter( (el) => {
-    return ( !_.isNull(el) && !_.isUndefined(el) )
-  });
-
-  let root: DomElem
-  let currElem
-  for (let element of elements) {
-    if ( _.isString(element) ) {
-      currElem.content = <String> element
-      continue
-    } 
-
-    element = <DomElem>element
-
-    if ( _.isUndefined(currElem) ) {
-      root = element
-      currElem = element
-      continue
-    }
-
-    if ( element.level > currElem.level ) {
-      element.parent = currElem
-      currElem.childs.push(element)
-      currElem = element
-      continue
-    }
-
-    while (element.level < currElem.level) {
-      currElem = currElem.parent
-    }
-
-    element.parent = currElem.parent
-    currElem.parent.childs.push(element)
-    currElem = element
-
-  }
-
-  return root.render()
-}
-
 class DomElem {
-  childs: DomElem[] = []
-  parent: DomElem
+  private childs: DomElem[] = []
+  private parent: DomElem
   classes: string[]
   id: string
   otherParam: string[]
@@ -78,6 +30,51 @@ class DomElem {
     return `<${this.tag} ${param}>${ content }</${this.tag}>`
   }
 
+  addChild(child: DomElem){
+    child.parent = this
+    this.childs.push(child)
+  }
+
+  addChilds(slaveElems: DomElem[]) {
+    this.childs.push(...slaveElems)
+  }
+
+  getParent () {
+    return this.parent
+  }
+}
+
+export function vdom (str: string): string {
+  let root: DomElem
+  let lastElem: DomElem
+
+  let lines = str.split(/\r\n|\r|\n/);
+  for (let line of lines) {
+    let element = parseLine(line)
+    if ( _.isUndefined(element) ) 
+      continue
+
+    if ( _.isUndefined(lastElem) ) {
+      root = element
+      lastElem = element
+      continue
+    }
+
+    if ( element.level > lastElem.level ) {
+      lastElem.addChild(element)
+      lastElem = element
+      continue
+    }
+
+    while (element.level < lastElem.level) {
+      lastElem = lastElem.getParent()
+    }
+
+    lastElem.getParent().addChild(element)
+    lastElem = element
+  }
+
+  return root.render()
 }
 
 function spacesBeforeFirstCharacter(str: string): number {
@@ -85,35 +82,28 @@ function spacesBeforeFirstCharacter(str: string): number {
   return str.match(pttrn)[0].length;
 }
 
-function parseLine(line: string): DomElem|undefined|string {
+function parseLine(line: string): DomElem|undefined {
   let level = spacesBeforeFirstCharacter(line)
   if (level === 0) return
 
   line = line.trim()
   if (line.length === 0) return
+  if ( isComment(line) ) return
+
+  let { master, slaves } = getArray(line)
 
   let content: string
-  let lineArray = line.split('>') 
-  if ( lineArray.length > 1 ){
-    content = lineArray[1]
-  }
-
-  let tag: string
-  lineArray = lineArray[0].trim().split(' ')
-  if ( isClass(lineArray[0]) || isID(lineArray[0]) ) {
-    tag = "div"
+  if ( _.isUndefined(slaves) ) {
+    [line, content] = line.split('>')
   } else {
-    tag = lineArray.shift();
+    line = master
   }
 
-  if ( tag === "" ){
-    if ( !_.isUndefined(content) ) return content
-    debugger
-    throw new Error(`В шаблоне допущена ошибка: ${line}`)
-  }
+  let lineArray = line.trim().split(' ')
+  let tag = ( isTag(lineArray[0]) ) ? lineArray.shift() : "div"
 
   let classes: string[] = []
-  let other:string[] = []
+  let other: string[] = []
   let id: string
 
   lineArray.forEach(param => {
@@ -130,7 +120,30 @@ function parseLine(line: string): DomElem|undefined|string {
     other.push(param)
   })
 
-  return new DomElem(level, tag, id, classes, other, content)
+  let elem = new DomElem(level, tag, id, classes, other, content)
+  if ( !_.isUndefined(slaves) ) {
+    let slaveElems = slaves.map( slave => parseLine(slave) ) 
+    elem.addChilds(slaveElems)
+  }
+  return elem 
+}
+
+function isComment(str: string) {
+  let pttrn = /\s*\/\//;
+  return pttrn.test(str); 
+}
+
+function getArray(str: string) {
+  let pttrn = /(?<master>.+)\[(?<array>.+)\]/;
+  let match = str.match(pttrn)
+
+  let slaves: string[], master: string
+  if (match) {
+    slaves = match.groups.array.split(',')
+    master = match.groups.master
+  }
+
+  return { master, slaves }
 }
 
 function isClass(param: string) {
@@ -139,4 +152,8 @@ function isClass(param: string) {
 
 function isID(param: string) {
   return param[0] === '#'
+}
+
+function isTag(param: string) {
+  return !isClass(param) && !isID(param)
 }
